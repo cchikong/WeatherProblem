212a4c057427364cf7b921d52ba71f9625382b92
cchikong <cchikong@gmail.com>
2026-03-23 11:03:49 +0800

    Initial Demo Program for openweather

---
+++ src/testgit/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testgit;
+
+/**
+ *
+ * @author Chan
+ */
+public class NewClass {
+    static int x=3;
+    
+    public static void test() {
+        System.out.println(x);
+        
+    }
+    
+    
+}
